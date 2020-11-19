ab14f1e5e56a2c9ce3554238aa09ae5383babe1e
hialexis <38670794+hialexis@users.noreply.github.com>
2020-11-19 14:40:09 -0800

    Handle errors

--- venv/Spotupy.py
+++ venv/Spotupy.py
@@ -1,23 +1,28 @@
 import json
+
+import numpy as np
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+
 
 def run_credentials():
     with open("spotipy_credentials.json", "r") as file:
         creds = json.load(file)
 
     auth_manager = SpotifyClientCredentials(client_id=creds['SPOTIPY_CLIENT_ID'],
-                                        client_secret=creds['SPOTIPY_CLIENT_SECRET'])
+                                            client_secret=creds['SPOTIPY_CLIENT_SECRET'])
     sp = spotipy.Spotify(client_credentials_manager=auth_manager)
-    
+
     return sp
+
 
 def retrieve_album_id(album, artist):
     sp = run_credentials()
-    results = sp.search(q="album:" + album,type="album", limit=1)
+    results = sp.search(q="album:" + album, type="album", limit=1)
     album_id = results['albums']['items'][0]['uri']
     return album_id
+
 
 def retireve_track_names(album_id):
     sp = run_credentials()
@@ -25,27 +30,51 @@ def retireve_track_names(album_id):
     tracks = sp.album_tracks(album_id)
     for track in tracks['items']:
         track_names.append(track['name'])
-    
     return track_names
+
 
 def retrieve_track_ids(artist_name, track_names):
     tracks_ids = []
     sp = run_credentials()
     for i in range(0, len(track_names)):
         result = sp.search(q="artist:" + artist_name + " track:" + track_names[i], limit=1)
-        song_id = result['tracks']['items'][0]['uri']
-        tracks_ids.append(song_id)
-        
+
+        if bool(result['tracks']['items']) == True:
+            song_id = result['tracks']['items'][0]['uri']
+        else:
+            song_id = np.nan
+
+    tracks_ids.append(song_id)
+
     return tracks_ids
+
 
 def retrieve_audio_features(track_names, tracks_ids):
     sp = run_credentials()
-    audio_features = sp.audio_features(tracks=tracks_ids)
-    datframe = pd.DataFrame(sp.audio_features(tracks=tracks_ids))
-    datframe = datframe.select_dtypes(['number'])
-    datframe = pd.DataFrame(datframe.sum(axis=0) / len(track_names))
+    column_names = ["acousticness",
+                    "danceability",
+                    "duration_ms",
+                    "energy",
+                    "instrumentalness",
+                    "key",
+                    "liveness",
+                    "loudness",
+                    "mode",
+                    "speechiness",
+                    "tempo",
+                    "time_signature",
+                    "valence",
+                    "break"
+                    ]
+    if pd.isnull(tracks_ids) == False:
+        audio_features = sp.audio_features(tracks=tracks_ids)
+        datframe = pd.DataFrame(sp.audio_features(tracks=tracks_ids))
+        datframe = datframe.select_dtypes(['number'])
+        datframe = pd.DataFrame(datframe.sum(axis=0) / len(track_names))
 
-    avg_data = pd.DataFrame()
-    avg_data = avg_data.append(datframe[0], ignore_index=True)
-    
+        avg_data = pd.DataFrame()
+        avg_data = avg_data.append(datframe[0], ignore_index=True)
+    else:
+        avg_data = pd.DataFrame(columns=column_names)
+        print(avg_data)
     return avg_data
--- venv/main.py
+++ venv/main.py
@@ -1,21 +1,27 @@
-import pandas as pd
-import database
-from database import *
 import Spotupy
+import database
+import pandas as pd
 import spotipy
+from database import *
 from spotipy.oauth2 import SpotifyClientCredentials
 
 initial = database.run_query(sqlfile='initial_data.sql')
 
-album = initial[0][1]
-artist = initial[0][2]
+"""try:
+"""
+for x in range(0, len(initial)):
+        album = initial[x][1]
+        artist = initial[x][2]
+        Spotupy.run_credentials()
 
-Spotupy.run_credentials()
-
-album_id = Spotupy.retrieve_album_id(album, artist)
-track_names = Spotupy.retireve_track_names(album_id)
-track_id = Spotupy.retrieve_track_ids(artist,track_names)
-
-audio_features = Spotupy.retrieve_audio_features(track_names, track_id)
-
-print(audio_features)
+        album_id = Spotupy.retrieve_album_id(album, artist)
+        track_names = Spotupy.retireve_track_names(album_id)
+        track_id = Spotupy.retrieve_track_ids(artist, track_names)
+        
+        audio_features = Spotupy.retrieve_audio_features(track_names, track_id)
+        
+"""except Exception as ex:
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}. The problem lies with the " + album + " by " + artist
+    message = template.format(type(ex).__name__, ex.args)
+    print(message)
+"""
